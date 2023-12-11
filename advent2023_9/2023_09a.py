4890a81e1af2592a42a90e6e3a24858a0ccaa4aa
Joep Weijers <j.weijers@topdesk.com>
2023-12-11 19:05:25 +0100

    Day 9

---
+++ advent2023_9/2023_09a.py
@@ -0,0 +1,24 @@
+def strings_to_ints(list):
+    return [int(x) for x in list]
+
+f = open('input9.txt', 'r')
+lines = f.read().splitlines()
+
+answer = 0
+for line in lines:
+    history = strings_to_ints(line.split())
+    diffs = []
+    list = history
+    to_add = history[-1]
+    while not all(y == 0 for y in list):
+        diff = []
+        for i in range(1, len(list)):
+            delta = list[i] - list[i - 1]
+            diff.append(delta)
+
+        diffs.append(diff)
+        list = diff
+        to_add += diff[-1]
+    answer += to_add
+
+print(answer)
